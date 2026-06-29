cc598b8fa5e890787232c63634b19e8c7dfb30bc
Justin <jt.clayton92@yahoo.com>
2026-06-28 19:59:42 -0700

    Markdown tab size

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -5,3 +5,12 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 		vim.hl.on_yank()
 	end,
 })
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "markdown",
+	callback = function()
+		vim.opt.shiftwidth = 2
+		vim.opt.tabstop = 2
+		vim.opt.softtabstop = 2
+	end,
+})
